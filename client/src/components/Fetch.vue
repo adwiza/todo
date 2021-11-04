93442da5cb2961d2e6c1c96a0215abdbaaa39c3d
Adwiz <adwiz@suroviy.ru>
2021-11-04 19:04:45 +0300

    init commit

---
+++ client/src/components/Fetch.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <p>{{ msg }}</p>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+const dataURL = 'https://api.myjson.com/bins/10dxnx';
+
+export default {
+  name: 'Fetch',
+  data() {
+    return {
+      msg: 'Запрос ещё не прошёл',
+    };
+  },
+  methods: {
+    getMessage() {
+      axios.get(dataURL)
+        .then((response) => {
+          // console.log(response);
+          this.msg = response.data.message;
+        });
+    },
+  },
+  created() {
+    // this.getMessage();
+    axios.get(dataURL)
+      .then((response) => {
+        // console.log(response);
+        this.msg = response.data.message;
+      });
+  },
+};
+</script>
